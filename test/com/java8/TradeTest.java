1b99fb61157330b6a1e37af35674be1b6d8e4a67
alexandregama <alexandre.gama.lima@gmail.com>
2015-09-05 14:05:54 -0300

    Merge two trades using lambda expression

--- test/com/java8/TradeTest.java
+++ test/com/java8/TradeTest.java
@@ -25,4 +25,19 @@ public class TradeTest {
 		assertEquals(25, finalTrade.getQuantity(), 0);
 	}
 	
+	@Test
+	public void shouldMergeTwoTradesUsingLambdaExpression() throws Exception {
+		IAddable<Trade> mergedTrade = (Trade t1, Trade t2) -> {
+			t1.setQuantity(t1.getQuantity() + t2.getQuantity());
+			return t1;
+		};
+		
+		Trade trade1 = new Trade(10, "First Trade");
+		Trade trade2 = new Trade(15, "Second Trade");
+		
+		Trade finalTrade = mergedTrade.add(trade1, trade2);
+		
+		assertEquals(25, finalTrade.getQuantity(), 0);
+	}
+	
 }
